94d16809f49326d556d448ea5cb8e3760fa139b2
Evilpasture <245716558+Evilpasture@users.noreply.github.com>
2026-07-17 08:38:53 +0700

    add more undefs

--- src/render/RenderingPCH.h
+++ src/render/RenderingPCH.h
@@ -16,6 +16,8 @@
 // These headers define global macros such as "None", "Success", "Bool", and "Status",
 // which pollute the namespace and conflict with our clean C++ enums/classes.
 // We are reclaiming the English language for ourselves. Glory to our namespaces.
+
+// --- Standard X11 Conflict Cleanups ---
 #ifdef None
 #undef None
 #endif
@@ -27,4 +29,67 @@
 #endif
 #ifdef Status
 #undef Status
+#endif
+#ifdef True
+#undef True
+#endif
+#ifdef False
+#undef False
+#endif
+#ifdef Always
+#undef Always
+#endif
+
+// --- Physics Engine & Geometry Conflict Cleanups ---
+#ifdef Convex
+#undef Convex
+#endif
+#ifdef Nonconvex
+#undef Nonconvex
+#endif
+#ifdef Complex
+#undef Complex
+#endif
+
+// --- Image/Texture & Allocation Conflict Cleanups ---
+#ifdef MappingSuccess
+#undef MappingSuccess
+#endif
+#ifdef MappingBusy
+#undef MappingBusy
+#endif
+#ifdef MappingFailed
+#undef MappingFailed
+#endif
+#ifdef Unsorted
+#undef Unsorted
+#endif
+#ifdef GrayScale
+#undef GrayScale
+#endif
+
+// --- Window, Layout, & Event Conflict Cleanups ---
+#ifdef KeyPress
+#undef KeyPress
+#endif
+#ifdef KeyRelease
+#undef KeyRelease
+#endif
+#ifdef FocusIn
+#undef FocusIn
+#endif
+#ifdef FocusOut
+#undef FocusOut
+#endif
+#ifdef FontChange
+#undef FontChange
+#endif
+#ifdef CursorShape
+#undef CursorShape
+#endif
+#ifdef Above
+#undef Above
+#endif
+#ifdef Below
+#undef Below
 #endif
